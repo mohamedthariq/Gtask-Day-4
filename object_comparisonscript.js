1ce88056bf3436aeea3a4824823018c622008ef7
mohamedthariq <mohamedthariq1997@gmail.com>
2022-10-20 23:14:57 +0530

    Add files via upload
    Object comparison

---
+++ object_comparisonscript.js
@@ -0,0 +1,30 @@
+
+
+//compare two json object
+
+var obj1 = { name:"Person 1", age:33 };
+var obj2 = { age:32, name:"Person 1" };
+
+var flag=true;
+
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+   
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
+
+}
+else {
+    flag=false;
+}
+console.log("is object equal - "+flag);
+
+//Output
+
+//true
